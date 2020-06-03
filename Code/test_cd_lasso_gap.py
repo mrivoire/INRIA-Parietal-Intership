11206ecbdd9ea7796f7c552ccb7e6e08a0de7d64
Manon Rivoire <manon.rivoire@devinci.fr>
2020-06-03 12:12:15 +0200

    test for the convergence of the duality gap to 0

---
+++ Code/test_cd_lasso_gap.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+from cd_solver_lasso import simu, cyclic_coordinate_descent
+
+
+def test_cd_lasso_gap():
+    # Data Simulation
+    rng = np.random.RandomState(0)
+    n_samples, n_features = 10, 30
+    beta = rng.randn(n_features)
+    lmbda = 0.1
+
+    X, y = simu(beta, n_samples=n_samples, corr=0.5, for_logreg=False)
+
+    epsilon = 1e-14
+    f = 10
+
+    (beta_hat_cyclic_cd_false,
+        primal_hist,
+        dual_hist,
+        gap_hist,
+        r_list,
+        n_active_features_true,
+        objs_cyclic_cd,
+        theta_hat_cyclic_cd,
+        P_lmbda,
+        D_lmbda,
+        G_lmbda) = cyclic_coordinate_descent(X,
+                                             y,
+                                             lmbda,
+                                             epsilon,
+                                             f,
+                                             n_epochs=4000,
+                                             screening=False)
+
+    assert G_lmbda < 1e-12
