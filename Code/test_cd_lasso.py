41ab9749293da32d4e39147e28db48646b320684
Manon Rivoire <manon.rivoire@devinci.fr>
2020-06-03 12:40:03 +0200

    file gathering all the tests

--- Code/test_cd_lasso.py
+++ Code/test_cd_lasso.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from cd_solver_lasso import simu, cyclic_coordinate_descent
+from sklearn.linear_model import Lasso as sklearn_Lasso
 
 
 def test_cd_lasso_gap():
@@ -34,3 +35,92 @@ def test_cd_lasso_gap():
                                              screening=False)
 
     assert G_lmbda < 1e-12
+
+
+def test_KKT_conditions():
+    """Test whether the KKT conditions are satisfied or not
+
+    Parameters
+    ----------
+    parameters are given by the @pytest
+
+    Returns
+    -------
+    None
+    """
+
+    rng = np.random.RandomState(0)
+    n_samples, n_features = 10, 30
+    beta = rng.randn(n_features)
+    epsilon = 10**(-14)
+    lmbda = 0.1
+    f = 10
+
+    X, y = simu(beta, n_samples=n_samples, corr=0.5, for_logreg=False)
+
+    (beta_hat_cyclic_cd_true, _, _, _, _, _, _, _, _, _, _) = \
+        cyclic_coordinate_descent(X, y, lmbda, epsilon, f, n_epochs=1000,
+                                  screening=True)
+
+    kkt = abs(np.dot(X.T, y - np.dot(X, beta_hat_cyclic_cd_true)))
+
+    np.testing.assert_allclose(kkt, lmbda, rtol=1)
+
+
+def test_radius_convergence():
+    """Test whether the radius converges towards 0
+
+    Parameters
+    ----------
+    parameters are given by the @pytest
+
+    Returns
+    -------
+    None
+    """
+    rng = np.random.RandomState(0)
+    n_samples, n_features = 10, 30
+    beta = rng.randn(n_features)
+    epsilon = 10**(-14)
+    lmbda = 0.1
+    f = 10
+
+    X, y = simu(beta, n_samples=n_samples, corr=0.5, for_logreg=False)
+
+    (_, _, _, _, r_list, _, _, _, _, _, G_lmbda) = \
+        cyclic_coordinate_descent(X, y, lmbda, epsilon, f, n_epochs=1000,
+                                  screening=True)
+
+    assert r_list[-1] < 1
+
+
+def test_lasso():
+    """Test that our Lasso solver behaves as sklearn's Lasso
+
+    Parameters
+    ----------
+    parameters are given thanks to @pytest
+
+    Returns
+    -------
+    None
+    """
+    rng = np.random.RandomState(0)
+    n_samples, n_features = 10, 30
+    beta = rng.randn(n_features)
+    epsilon = 10**(-14)
+    lmbda = 0.1
+    f = 10
+
+    X, y = simu(beta, n_samples=n_samples, corr=0.5, for_logreg=False)
+
+    # Sklearn's Lasso
+    lasso = sklearn_Lasso(alpha=lmbda / len(X), fit_intercept=False,
+                          normalize=False, tol=1e-10).fit(X, y)
+
+    # Our Lasso
+    (beta_hat_cyclic_cd_true, _, _, _, _, _, _, _, _, _, _) = \
+        cyclic_coordinate_descent(X, y, lmbda, epsilon, f, n_epochs=10000,
+                                  screening=True)
+
+    np.testing.assert_allclose(beta_hat_cyclic_cd_true, lasso.coef_, rtol=1e-05)
