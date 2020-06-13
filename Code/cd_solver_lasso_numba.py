0e9e31fd34ae5e3abf03758cca2c7955a99d83d9
Manon Rivoire <manon.rivoire@devinci.fr>
2020-06-13 15:22:27 +0200

    Lasso class

--- Code/cd_solver_lasso_numba.py
+++ Code/cd_solver_lasso_numba.py
@@ -13,7 +13,7 @@ from sklearn.tree import DecisionTreeRegressor
 from sklearn.decomposition import PCA
 from sklearn.model_selection import cross_val_score
 from sklearn.linear_model import Lasso as sklearn_Lasso
-from sklearn.metrics import mean_absolute_error
+
 
 ######################################################################
 #     Iterative Solver With Gap Safe Rules
@@ -57,162 +57,266 @@ def simu(beta, n_samples=1000, corr=0.5, for_logreg=False):
     return X, y
 
 
-##############################################################################
-#   Minimization of the Primal Problem with Coordinate Descent Algorithm
-##############################################################################
+###########################################################################
+#                           Class Lasso
+###########################################################################
 
-@njit
-def cyclic_coordinate_descent(X, y, lmbda, epsilon, f, n_epochs=5000,
-                              screening=True, store_history=True):
-    """Solver : cyclic coordinate descent
+@njit 
+class Lasso:
+    """Defines the functions required for the Lasso optimization problem
 
     Parameters
     ----------
-
-    X: numpy.ndarray, shape (n_samples, n_features)
-        features matrix
-
-    y: numpy.array, shape (n_samples, )
-        target labels vector
-
-    lmbda: float
-        regularization parameter
-
-    epsilon: float
-        stopping criterion
-
-    f: int
-       frequency
-
-    n_epochs: int,
-
-    screening: bool, default = True
-        defines whether or not one adds screening to the solver
-
-    store_history: bool, default = True
-        defines whether or not one stores the values of the parameters
-        while the solver is running
-
-    Returns
-    -------
-    beta: numpy.array, shape(n_features,)
-        primal parameters vector
-
-    theta: numpy.array, shape(n_samples, )
-        dual parameters vector
-
-    P_lmbda: float
-        primal value
-
-    D_lmbda: float
-        dual value
-
-    primal_hist: numpy.array, shape(n_epochs / f, )
-        store the primal values during the whole solving process
-
-    dual_hist: numpy.array, shape(n_epochs / f, )
-        store the dual values during the whole solving process
-
-    gap_hist: numpy.array, shape(n_epochs / f, )
-        store the duality gap values during the whole solving process
-
-    r_list: numpy.array, shape(n_epochs / f, )
-        store the values of the radius of the safe sphere during
-        the screening process
-
-    n_active_features: numpy.array, shape(n_epochs / f, )
-        store the number of active features in the active set
-        during the screening process
-
     """
+    @njit 
+    def __init__(self, lmbda, penalty, epsilon, f, n_epochs, solver, screening, 
+                 store_history):
 
-    # Initialisation of the parameters
+        self.lmbda = lmbda
+        self.penalty = penalty
+        self.epsilon = epsilon
+        self.f = f
+        self.n_epochs = n_epochs
+        self.solver = solver
+        self.screening = screening
+        self.store_history = store_history
 
-    n_samples, n_features = X.shape
+        assert epsilon > 0
+        assert self.penalty in ['l1']
+        assert self.solver in ['cyclic_coordinate_descent']
 
-    beta = np.zeros(n_features)
-    theta = np.zeros(n_samples)
+    @njit
+    def cyclic_coordinate_descent(self, X, y):
+        """Solver : cyclic coordinate descent
 
-    n_active_features = []
-    r_list = []
-    primal_hist = []
-    dual_hist = []
-    gap_hist = []
-    theta_hist = []
+        Parameters
+        ----------
 
-    residuals = y - X.dot(beta)
+        X: numpy.ndarray, shape (n_samples, n_features)
+            features matrix
 
-    # Computation of the lipschitz constants vector
+        y: numpy.array, shape (n_samples, )
+            target labels vector
 
-    L = (X**2).sum(0)
+        lmbda: float
+            regularization parameter
 
-    A_c = list(range(n_features))
+        epsilon: float
+            stopping criterion
 
-    # Iterations of the algorithm
-    for k in range(n_epochs):
-        for i in A_c:
-            # One cyclicly updates the i^{th} coordinate corresponding to the
-            # rest in the Euclidean division by the number of features
-            # This allows to always selecting an index between 1 and n_features
-            old_beta_i = beta[i]
-            step = 1 / L[i]
-            grad = np.dot(X[:, i], residuals)
+        f: int
+        frequency
 
-            # Update of the parameters
-            beta[i] += step * grad
+        n_epochs: int,
 
-            # Apply proximal operator
-            beta[i] = soft_thresholding(step * lmbda, beta[i])
+        screening: bool, default = True
+            defines whether or not one adds screening to the solver
 
-            # Update of the residuals
-            if old_beta_i != beta[i]:
-                residuals += (old_beta_i - beta[i]) * X[:, i]
+        store_history: bool, default = True
+            defines whether or not one stores the values of the parameters
+            while the solver is running
 
-        if k % f == 0:
-            # Computation of theta
-            theta = (residuals
-                     / (lmbda * max(np.max(np.abs(residuals/lmbda)), 1)))
+        Returns
+        -------
+        beta: numpy.array, shape(n_features,)
+            primal parameters vector
 
-            # Computation of the primal problem
-            P_lmbda = 0.5 * residuals.dot(residuals)
-            P_lmbda += lmbda * np.linalg.norm(beta, 1)
+        theta: numpy.array, shape(n_samples, )
+            dual parameters vector
 
-            # Computation of the dual problem
-            D_lmbda = 0.5*np.linalg.norm(y, ord=2)**2
-            D_lmbda -= (((lmbda**2) / 2)
-                        * np.linalg.norm(theta - y / lmbda, ord=2)**2)
+        P_lmbda: float
+            primal value
 
-            # Computation of the dual gap
-            G_lmbda = P_lmbda - D_lmbda
+        D_lmbda: float
+            dual value
 
-            # Objective function related to the primal
-            if store_history:
-                theta_hist.append(theta)
-                primal_hist.append(P_lmbda)
-                dual_hist.append(D_lmbda)
-                gap_hist.append(G_lmbda)
+        primal_hist: numpy.array, shape(n_epochs / f, )
+            store the primal values during the whole solving process
 
-            if screening:
-                # Computation of the radius of the gap safe sphere
-                r = np.sqrt(2*np.abs(G_lmbda))/lmbda
-                # r_list.append(r)
+        dual_hist: numpy.array, shape(n_epochs / f, )
+            store the dual values during the whole solving process
 
-                # Computation of the active set
-                for j in A_c:
-                    # mu = mu_B(X[:, j], theta, r)
-                    mu = (np.abs(np.dot(X[:, j].T, theta))
-                          + r * np.linalg.norm(X[:, j]))
-                    if mu < 1:
-                        A_c.remove(j)
-                if store_history:
-                    n_active_features.append(len(A_c))
-                    r_list.append(r)
+        gap_hist: numpy.array, shape(n_epochs / f, )
+            store the duality gap values during the whole solving process
 
-                if np.abs(G_lmbda) <= epsilon:
-                    break
+        r_list: numpy.array, shape(n_epochs / f, )
+            store the values of the radius of the safe sphere during
+            the screening process
 
-    return (beta, primal_hist, dual_hist, gap_hist, r_list,
-            n_active_features, theta, P_lmbda, D_lmbda, G_lmbda)
+        n_active_features: numpy.array, shape(n_epochs / f, )
+            store the number of active features in the active set
+            during the screening process
+
+        """
+
+        # Initialisation of the parameters
+
+        n_samples, n_features = np.shape(X)
+
+        beta = np.zeros(n_features)
+        theta = np.zeros(n_samples)
+
+        n_active_features = []
+        r_list = []
+        primal_hist = []
+        dual_hist = []
+        gap_hist = []
+        theta_hist = []
+
+        residuals = y - X.dot(beta)
+
+        # Computation of the lipschitz constants vector
+
+        L = (X**2).sum(0)
+
+        A_c = list(range(n_features))
+
+        # Iterations of the algorithm
+        for k in range(self.n_epochs):
+            for i in A_c:
+                # One cyclicly updates the i^{th} coordinate corresponding to
+                # the rest in the Euclidean division by the number of features
+                # This allows to always selecting an index between 1 and
+                # n_features
+
+                old_beta_i = beta[i]
+                step = 1 / L[i]
+                grad = np.dot(X[:, i], residuals)
+
+                # Update of the parameters
+                beta[i] += step * grad
+
+                # Apply proximal operator
+                beta[i] = soft_thresholding(step * self.lmbda, beta[i])
+
+                # Update of the residuals
+                if old_beta_i != beta[i]:
+                    residuals += (old_beta_i - beta[i]) * X[:, i]
+
+            if k % self.f == 0:
+                # Computation of theta
+                theta = (residuals
+                         / (self.lmbda * max(np.max(np.abs(residuals
+                                                    / self.lmbda)), 1)))
+
+                # Computation of the primal problem
+                P_lmbda = 0.5 * residuals.dot(residuals)
+                P_lmbda += self.lmbda * np.linalg.norm(beta, 1)
+
+                # Computation of the dual problem
+                D_lmbda = 0.5*np.linalg.norm(y, ord=2)**2
+                D_lmbda -= (((self.lmbda**2) / 2)
+                            * np.linalg.norm(theta - y / self.lmbda, ord=2)**2)
+
+                # Computation of the dual gap
+                G_lmbda = P_lmbda - D_lmbda
+
+                # Objective function related to the primal
+                if self.store_history:
+                    theta_hist.append(theta)
+                    primal_hist.append(P_lmbda)
+                    dual_hist.append(D_lmbda)
+                    gap_hist.append(G_lmbda)
+
+                if self.screening:
+                    # Computation of the radius of the gap safe sphere
+                    r = np.sqrt(2*np.abs(G_lmbda))/self.lmbda
+                    # r_list.append(r)
+
+                    # Computation of the active set
+                    for j in A_c:
+                        # mu = mu_B(X[:, j], theta, r)
+                        mu = (np.abs(np.dot(X[:, j].T, theta))
+                              + r * np.linalg.norm(X[:, j]))
+                        if mu < 1:
+                            A_c.remove(j)
+                    if self.store_history:
+                        n_active_features.append(len(A_c))
+                        r_list.append(r)
+
+                    if np.abs(G_lmbda) <= self.epsilon:
+                        break
+
+        return (beta, primal_hist, dual_hist, gap_hist, r_list,
+                n_active_features, theta, P_lmbda, D_lmbda, G_lmbda)
+
+    @njit
+    def fit(self, X, y):
+        """Fit the data (X,y) based on the solver of the Lasso class
+
+        Parameters
+        ----------
+
+        X: numpy.ndarray, shape = (n_samples, n_features)
+            features matrix
+
+        y: numpy.array, shape = (n_samples, )
+            target vector
+
+        Returns
+        -------
+        self: Lasso object
+        """
+
+        (beta_hat_cyclic_cd_true,
+         primal_hist,
+         dual_hist,
+         gap_hist,
+         r_list,
+         n_active_features_true,
+         theta_hat_cyclic_cd,
+         P_lmbda,
+         D_lmbda,
+         G_lmbda) = self.cyclic_coordinate_descent(X, y)
+
+        self.params = beta_hat_cyclic_cd_true
+        self.slopes = beta_hat_cyclic_cd_true[:1]
+        self.intercept = beta_hat_cyclic_cd_true[0]
+
+        return self
+
+    @njit
+    def predict(self, X):
+        """Predict the target from the observations matrix
+
+        Parameters
+        ----------
+        X: numpy.ndarray, shape = (n_samples, n_features)
+            features matrix
+
+        Returns
+        -------
+        y_hat: numpy.array, shape = (n_samples, )
+            predicted target vector
+        """
+
+        y_hat = np.dot(X, self.slopes) + self.intercept
+
+        return y_hat
+
+    @njit
+    def score(self, X, y):
+        """Compute the cross-validation score to assess the performance of the
+           model (use negative mean absolute error)
+
+        Parameters
+        ----------
+        X: numpy.ndarray, shape = (n_samples, n_features)
+            features matrix
+
+        y: numpy.array, shape = (n_samples, )
+            target vector
+
+        Returns
+        -------
+        score: float
+            negative mean absolute error (MAE)
+            negative to keep the semantic that higher is better
+        """
+
+        score = -np.mean(np.abs(y - self.predict(X)))
+
+        return score
 
 
 ##########################################################
@@ -268,16 +372,6 @@ def soft_thresholding(u, x):
     return ST
 
 
-#########################################
-#               RMSE
-#########################################
-
-def rmse_cv(model, X, y, scoring="neg_mean_squared_error", cv=5):
-    """RMSE : Cross-val score 
-    """
-    rmse = np.sqrt(-cross_val_score(model, X, y, scoring, cv).mean())
-    return(rmse)
-
 #######################################
 #              Read CSV
 #######################################
@@ -293,52 +387,29 @@ def main():
     np.random.seed(0)
     n_samples, n_features = 10, 30
     beta = np.random.randn(n_features)
-    lmbda = 0.1
 
     X, y = simu(beta, n_samples=n_samples, corr=0.5, for_logreg=False)
     print("number of samples :", X.shape[0])
     print("number of features :", X.shape[1])
 
-    # Minimization of the Primal Problem with Coordinate Descent Algorithm
-    epsilon = 10**(-20)
-    f = 10
-
-    (beta_hat_cyclic_cd_false,
-        primal_hist,
-        dual_hist,
-        gap_hist,
-        r_list,
-        n_active_features_true,
-        theta_hat_cyclic_cd,
-        P_lmbda,
-        D_lmbda,
-        G_lmbda) = cyclic_coordinate_descent(X,
-                                             y,
-                                             lmbda,
-                                             epsilon,
-                                             f,
-                                             n_epochs=10000,
-                                             screening=False,
-                                             store_history=True)
+    lasso = Lasso(lmbda=1., penalty='l1', epsilon=1., f=10, n_epochs=5000,
+                  solver='cyclic_coordinate_descent', screening=True,
+                  store_history=True)
 
     (beta_hat_cyclic_cd_true,
-        primal_hist,
-        dual_hist,
-        gap_hist,
-        r_list,
-        n_active_features_true,
-        theta_hat_cyclic_cd,
-        P_lmbda,
-        D_lmbda,
-        G_lmbda) = cyclic_coordinate_descent(X,
-                                             y,
-                                             lmbda,
-                                             epsilon,
-                                             f,
-                                             n_epochs=10000,
-                                             screening=True,
-                                             store_history=True)
+     primal_hist,
+     dual_hist,
+     gap_hist,
+     r_list,
+     n_active_features_true,
+     theta_hat_cyclic_cd,
+     P_lmbda,
+     D_lmbda,
+     G_lmbda) = lasso.cyclic_coordinate_descent(X, y)
 
+    print("beta hat : ", beta_hat_cyclic_cd_true)
+
+    """
     # Plot primal objective function (=primal_hist)
     obj = primal_hist
 
@@ -413,7 +484,7 @@ def main():
 
     # cd = cyclic_coordinate_descent(X, y, lmbda, epsilon, f, n_epochs=10000,
     #                                screening=False,
-    #                                store_history=True)[0].fit(X, y)
+    #                                store_history=True).fit(X, y)
 
     ax1.plot(X[:, 0], reg.predict(X), linewidth=2, color='blue',
              label='linear regression')
@@ -439,10 +510,10 @@ def main():
     # Linear Regression on the discretized dataset
     binning_reg = LinearRegression().fit(X_binned, y)
     # Lasso onthe discretized dataset
-    # binning_lasso = sklearn_Lasso(alpha=lmbda / len(X_binned),
-    #                               fit_intercept=False, normalize=False,
-    #                               max_iter=n_epochs,
-    #                               tol=1e-15).fit(X_binned.getnnz(axis=0), y)
+    binning_lasso = sklearn_Lasso(alpha=lmbda / X_binned.shape[0],
+                                  fit_intercept=False, normalize=False,
+                                  max_iter=n_epochs,
+                                  tol=1e-15).fit(X_binned, y)
 
     # binning_cd = cyclic_coordinate_descent(X_binned, y, lmbda, epsilon, f,
     #                                        n_epochs=10000, screening=False,
@@ -471,20 +542,20 @@ def main():
     std_original_crossval = cross_val_score(reg, X, y, cv=5).std()
     print("Original crossval score : ", original_crossval_score)
     print("Std original crossval score : ", std_original_crossval)
-    original_rmse = rmse_cv(reg, X, y, scoring="neg_mean_squared_error", cv=5)
-    print("RMSE on original dataset : ", original_rmse)
-    # mae_original_score = mean_absolute_error(y, reg)
-    # print("MAE original dataset : ", mae_original_score)
+    original_lasso_crossval_score = cross_val_score(lasso, X, y, cv=5).mean()
+    std_original_lasso_crossval = cross_val_score(lasso, X, y, cv=5).std()
+    print("Original Lasso crossval score : ", original_lasso_crossval_score)
+    print("Std original crossval score : ", std_original_lasso_crossval)
     binning_crossval_score = cross_val_score(reg, X_binned, y, cv=5).mean()
     std_binning_crossval = cross_val_score(reg, X_binned, y, cv=5).std()
     print("Binning crossval score : ", binning_crossval_score)
     print("Std binning crossval score : ", std_binning_crossval)
-    # mae_binning_score = mean_absolute_error(y, binning_reg)
-    # print("MAE binning score : ", mae_binning_score)
-    # cd_crossval_score = cross_val_score(cd, X, y, cv=5)
-    # print("CD crossval score : ", cd_crossval_score)
-    # binning_cd_crossval_score = cross_val_score(binning_cd, X_binned, y, cv=5)
-    # print("Binning CD crossval score : ", binning_cd_crossval_score)
+    binning_lasso_crossval_score = cross_val_score(binning_lasso,
+                                                   X_binned, y, cv=5).mean()
+    std_binning_lasso_crossval = cross_val_score(binning_lasso,
+                                                 X_binned, y, cv=5).std()
+    print("Binning Lasso crossval score : ", binning_lasso_crossval_score)
+    print("Std binning Lasso crossval score : ", std_binning_lasso_crossval)
 
     # PCA : Principal Component Analysis on the original dataset
     pca = PCA(n_components=2, svd_solver='full')
@@ -561,7 +632,7 @@ def main():
     head_test = test_set.head()
     print("Housing Prices Training Set Header : ", head_train)
     print("Housing Prices Testing Set Header : ", head_test)
-
+    """
 
 if __name__ == "__main__":
     main()
