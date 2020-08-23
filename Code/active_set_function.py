906d19ad14a68c379d0abd5d52e248c9bcd67cd4
Manon Rivoire <manon.rivoire@devinci.fr>
2020-08-23 17:16:05 +0200

    build X_active_set from dict

--- Code/active_set_function.py
+++ Code/active_set_function.py
@@ -35,10 +35,36 @@ def active_set_matrix(X_binned, active_set_keys):
 
     count = 0
     for key in active_set_keys:
-    # for j in range(n_active_features):
-        # X_active_set[:, count] = X_binned[:, j]
         X_active_set[:, count] = X_binned[:, key]  
         count += 1
+
+    return X_active_set
+
+
+def active_set_matrix_2(dict_active_set):
+    """
+    Parameters
+    ----------
+    X_binned: np.ndarray(n_samples, n_features * n_bins)
+        sparse matrix containing the new discrete features (one-hot vectors)
+
+    dict_active_set: dictionary
+        the keys of the dictionary are the keys of the active features 
+        (list of indices) and the corresponding values are the binned features
+        (one-hot encoded vectors)
+
+    Returns
+    -------
+    X_active_set: np.ndarray(n_samples, n_active_features)
+        sparse matrix containing only the discrete active features
+    """
+
+    X_active_set = []
+
+    for ind, key in enumerate(dict_active_set):
+        X_active_set.append(dict_active_set[key])
+
+    X_active_set = np.array(X_active_set)
 
     return X_active_set
 
@@ -51,13 +77,25 @@ def main():
                 [0, 0, 0, 1, 0],
                 [0, 0, 0, 0, 1]])
 
-    active_set_keys = [[1, 1], [2, 3], [1, 4]]
+    active_set_keys = {
+        
+    }
+
+    active_set_keys = [1, 3, 4]
 
     X_active_set = active_set_matrix(X_binned=X_binned, 
                                      active_set_keys=active_set_keys)
 
     print("X_active_set = ", X_active_set)
 
+    dict_active_set = {'[1, 2]': [0, 1, 0, 1, 0], 
+                       '[0, 3]': [1, 0, 1, 0, 1],
+                       '[1, 4]': [1, 1, 0, 0, 1]
+                       }
+
+    X_active_set_2 = active_set_matrix_2(dict_active_set=dict_active_set)
+
+    print("X_active_set_2 = ", X_active_set_2)
 
 if __name__ == "__main__":
     main()
