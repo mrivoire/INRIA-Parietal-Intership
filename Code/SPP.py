da74e8435ba2b72b8b4ff4cc8ead9ac71e470637
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2020-08-26 16:30:59 +0200

    test ok

--- Code/SPP.py
+++ Code/SPP.py
@@ -705,6 +705,52 @@ def safe_prune_rec(X_binned_data, X_binned_indices, X_binned_indptr,
     current_key.pop()
 
 
+@njit
+def from_numbalists_tocsc(numbalist_data, numbalist_ind):
+    """
+    Parameters
+    ----------
+    numbalist_data: List([int])
+        numba list of lists of integers containing the non-zero elements 
+        of each feature the sparse matrix
+
+    numbalist_ind: list([int])
+        nubma list of lists of integers containing the row indices of the 
+        non-zero elements of each feature of the sparse matrix
+
+    Returns
+    -------
+    csc_data: list(int)
+        list of integers containing the non-zero elements of the sparse matrix
+        without separation between the features 
+
+    csc_ind: list(int)
+        list of integers containing the row indices of the non-zero elements
+        of the sparse matrix without separation between the features
+
+    csc_indptr: list(int)
+        list of integers containing the indices of each first non-zero element
+        of each feature in the csc_data vector
+
+    """
+    csc_data = []
+    csc_ind = []
+    csc_indptr = [0]
+
+    indptr = 0
+    for feat in numbalist_data:
+        indptr += len(feat)
+        csc_indptr.append(indptr)
+        for data in feat:
+            csc_data.append(data)
+
+    for ind_list in numbalist_ind:
+        for ind in ind_list:
+            csc_ind.append(ind)
+
+    return csc_data, csc_ind, csc_indptr
+
+
 # @njit
 def SPP(X_binned_data, X_binned_indices, X_binned_indptr, y,
         n_val_gs, max_depth, epsilon, f, n_epochs, screening=True,
--- Code/test_from_numbalists_tocsc.py
+++ Code/test_from_numbalists_tocsc.py
@@ -1,58 +1,9 @@
 import numpy as np
-import time
-import pytest
 
-from sklearn.preprocessing import KBinsDiscretizer
-from sklearn.utils import check_random_state
-from SPP import simu, max_val
+from SPP import from_numbalists_tocsc
+
 from numba.typed import List
 from numba import njit
-
-
-@njit
-def from_numbalists_tocsc(numbalist_data, numbalist_ind):
-    """
-    Parameters
-    ----------
-    numbalist_data: List([int])
-        numba list of lists of integers containing the non-zero elements 
-        of each feature the sparse matrix
-
-    numbalist_ind: list([int])
-        nubma list of lists of integers containing the row indices of the 
-        non-zero elements of each feature of the sparse matrix
-
-    Returns
-    -------
-    csc_data: list(int)
-        list of integers containing the non-zero elements of the sparse matrix
-        without separation between the features 
-
-    csc_ind: list(int)
-        list of integers containing the row indices of the non-zero elements
-        of the sparse matrix without separation between the features
-
-    csc_indptr: list(int)
-        list of integers containing the indices of each first non-zero element
-        of each feature in the csc_data vector
-
-    """
-    csc_data = []
-    csc_ind = []
-    csc_indptr = [0]
-
-    indptr = 0
-    for feat in numbalist_data:
-        indptr += len(feat)
-        csc_indptr.append(indptr)
-        for data in feat:
-            csc_data.append(data)
-
-    for ind_list in numbalist_ind:
-        for ind in ind_list:
-            csc_ind.append(ind)
-
-    return csc_data, csc_ind, csc_indptr
 
 
 @njit
@@ -65,13 +16,23 @@ def main():
     print("numbalist_ind = ", numbalist_ind)
 
     csc_data, csc_ind, csc_indptr = \
-        from_numbalists_tocsc(numbalist_data=numbalist_data, 
+        from_numbalists_tocsc(numbalist_data=numbalist_data,
                               numbalist_ind=numbalist_ind)
+
+    return csc_data, csc_ind, csc_indptr
+
+
+def test_from_numbalists_tocsc():
+    csc_data, csc_ind, csc_indptr = main()
 
     print("csc_data = ", csc_data)
     print("csc_ind = ", csc_ind)
     print("csc_indptr = ", csc_indptr)
 
-    
+    np.testing.assert_array_equal(csc_data, [1, 2, 4, 5, 6])
+    np.testing.assert_array_equal(csc_ind, [0, 3, 1, 2, 2])
+    np.testing.assert_array_equal(csc_indptr, [0, 2, 4, 5])
+
+
 if __name__ == "__main__":
     main()
