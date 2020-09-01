3489ffa47bdbe69ee05165d21b17939ee722abd1
Manon Rivoire <manon.rivoire@devinci.fr>
2020-09-01 17:55:52 +0200

    add interaction features to the matrix on which we run the lasso

--- Code/test_SPP.py
+++ Code/test_SPP.py
@@ -9,8 +9,8 @@ from sklearn.preprocessing import KBinsDiscretizer
 from sklearn.linear_model import Lasso as sklearn_Lasso
 from sklearn.utils import check_random_state
 from cd_solver_lasso_numba import Lasso, sparse_cd
-from SPP import simu, SPP 
-
+from SPP import simu, SPP, compute_interactions
+from scipy.sparse import csc_matrix
 
 def test_SPP():
 
@@ -46,11 +46,62 @@ def test_SPP():
     n_samples = max(X_binned_indices) + 1
 
     # Lasso 
-    sparse_lasso_sklearn = sklearn_Lasso(alpha=(lmbda / X_binned.shape[0]),
+    # We have to run the lasso of sklearn on the matrix containing the whole 
+    # set of features i.e. both the discrete features provided thanks to the 
+    # binning process of the original continuous features and the features of 
+    # interactions until the maximum order
+
+    X_tilde_data = []
+    X_tilde_data.extend(X_binned_data)
+    X_tilde_ind = []
+    X_tilde_ind.extend(X_binned_indices)
+    X_tilde_indptr = []
+
+    X_inter_feat_data = []
+    X_inter_feat_ind = []
+    X_inter_feat_indptr = []
+    X_inter_feat_indptr.append(0)
+
+    count = len(X_binned_indices)
+    n_inter_feats = 0
+    # Computation of the features of interactions
+    for i in range(n_features):
+        start_feat1, end_feat1 = X_binned_indptr[i: i + 2]
+        for j in range(i, n_features):
+            n_inter_feats += 1
+            start_feat2, end_feat2 = X_binned_indptr[j: j + 2]
+
+            inter_feat_data, inter_feat_ind = \
+                compute_interactions(
+                    data1=X_binned_data[start_feat1: end_feat1], 
+                    ind1=X_binned_indices[start_feat1: end_feat1], 
+                    data2=X_binned_data[start_feat2: end_feat2], 
+                    ind2=X_binned_indices[start_feat2: end_feat2])
+
+            X_inter_feat_data.extend(inter_feat_data)
+            X_inter_feat_ind.extend(inter_feat_ind)
+            X_inter_feat_indptr.append(count + j * len(X_inter_feat_data))
+
+    X_tilde_indptr.extend(X_binned_indptr)
+    X_tilde_indptr.extend(X_inter_feat_indptr)
+
+    n_tilde_feats = n_features + n_inter_feats
+    print('n_features = ', n_features)
+    print('n_inter_feats = ', n_inter_feats)
+    print('n_tilde_feats = ', n_tilde_feats)
+    print('length X_tilde_data = ', len(X_tilde_data))
+    print('lenght X_tilde_ind = ', len(X_tilde_ind))
+    print('length X_tilde_indptr = ', len(X_tilde_indptr))
+
+    print('X_tilde_indptr = ', X_tilde_indptr)
+    X_tilde = csc_matrix((X_tilde_data, (X_tilde_ind, X_tilde_indptr)), 
+                         shape=(n_samples, n_tilde_feats)).toarray()
+
+    sparse_lasso_sklearn = sklearn_Lasso(alpha=(lmbda / X_tilde.shape[0]),
                                          fit_intercept=False,
                                          normalize=False,
                                          max_iter=n_epochs,
-                                         tol=1e-14).fit(X_binned, y)
+                                         tol=1e-14).fit(X_tilde, y)
 
     beta_star_lasso = sparse_lasso_sklearn.coef_
     # residuals = y - X_binned.dot(beta_star)
@@ -76,10 +127,10 @@ def test_SPP():
     assert np.count_nonzero(beta_star) == len(active_set_keys_opt)
 
 
-# def main():
+def main():
 
-#     test_SPP()
+    test_SPP()
 
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
