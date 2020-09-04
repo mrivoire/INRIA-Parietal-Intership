2497c48edfa7541cb5ff430bd9a5fce965bed7bd
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2020-09-04 17:50:11 +0200

    pep8

--- Code/test_SPP.py
+++ Code/test_SPP.py
@@ -1,14 +1,14 @@
 import numpy as np
-import time
+# import time
 import pytest
 
-from scipy.linalg import toeplitz
-from numba import njit
-from numba.typed import List
+# from scipy.linalg import toeplitz
+# from numba import njit
+# from numba.typed import List
 from sklearn.preprocessing import KBinsDiscretizer
 from sklearn.linear_model import Lasso as sklearn_Lasso
 from sklearn.utils import check_random_state
-from cd_solver_lasso_numba import Lasso, sparse_cd
+# from cd_solver_lasso_numba import Lasso, sparse_cd
 from SPP import simu, SPP, compute_interactions, max_val
 from scipy.sparse import csc_matrix, hstack
 
@@ -16,7 +16,7 @@ from scipy.sparse import csc_matrix, hstack
 @pytest.mark.parametrize("seed", [0, 1, 2, 5, 10])
 def test_SPP(seed):
 
-    # Definition of the parameters 
+    # Definition of the parameters
     rng = check_random_state(1)
     n_samples, n_features = 100, 10
     beta = rng.randn(n_features)
@@ -48,10 +48,10 @@ def test_SPP(seed):
     n_features = len(X_binned_indptr) - 1
     n_samples = max(X_binned_indices) + 1
     print('n_features = ', n_features)
-    # Lasso 
-    # We have to run the lasso of sklearn on the matrix containing the whole 
-    # set of features i.e. both the discrete features provided thanks to the 
-    # binning process of the original continuous features and the features of 
+    # Lasso
+    # We have to run the lasso of sklearn on the matrix containing the whole
+    # set of features i.e. both the discrete features provided thanks to the
+    # binning process of the original continuous features and the features of
     # interactions until the maximum order
 
     X_inter_feat_data = []
@@ -63,7 +63,7 @@ def test_SPP(seed):
 
     # Computation of the features of interactions
     X_tilde_keys = []
-        
+
     for i in range(n_features):
         X_tilde_keys.append([i])
         start_feat1, end_feat1 = X_binned_indptr[i: i + 2]
@@ -85,9 +85,9 @@ def test_SPP(seed):
             X_inter_feat_ind.extend(inter_feat_ind)
             X_inter_feat_indptr.append(indptr)
 
-    X_interfeats = csc_matrix((X_inter_feat_data, X_inter_feat_ind, 
-                               X_inter_feat_indptr), 
-                               shape=(n_samples, n_inter_feats))
+    X_interfeats = csc_matrix(
+        (X_inter_feat_data, X_inter_feat_ind, X_inter_feat_indptr),
+        shape=(n_samples, n_inter_feats))
 
     X_tilde = hstack([X_binned, X_interfeats])
     X_tilde_data = X_tilde.data
@@ -104,13 +104,13 @@ def test_SPP(seed):
     n_tilde_feats = n_features + n_inter_feats
     print('n_tilde_feats = ', n_tilde_feats)
 
-    # (beta_star_lasso, residuals, primal_hist_sparse, dual_hist_sparse, 
-    #  gap_hist_sparse, r_list_sparse, n_active_features_true_sparse, 
-    #  theta_hat_cyclic_cd_sparse, P_lmbda_sparse, D_lmbda_sparse, 
+    # (beta_star_lasso, residuals, primal_hist_sparse, dual_hist_sparse,
+    #  gap_hist_sparse, r_list_sparse, n_active_features_true_sparse,
+    #  theta_hat_cyclic_cd_sparse, P_lmbda_sparse, D_lmbda_sparse,
     #  G_lmbda_sparse, safe_set_sparse) = \
-    #     sparse_cd(X_data=X_tilde_data, X_indices=X_tilde_ind, 
-    #               X_indptr=X_tilde_indptr, y=y, lmbda=lmbda, 
-    #               epsilon=epsilon, f=f, n_epochs=n_epochs, 
+    #     sparse_cd(X_data=X_tilde_data, X_indices=X_tilde_ind,
+    #               X_indptr=X_tilde_indptr, y=y, lmbda=lmbda,
+    #               epsilon=epsilon, f=f, n_epochs=n_epochs,
     #               screening=screening, store_history=store_history)
 
     sparse_lasso_sklearn = sklearn_Lasso(alpha=(lmbda / X_tilde.shape[0]),
@@ -126,9 +126,9 @@ def test_SPP(seed):
             active_set_keys_lasso.append(X_tilde_keys[i])
 
     solutions_dict = \
-        SPP(X_binned_data=X_binned_data, X_binned_indices=X_binned_indices, 
-            X_binned_indptr=X_binned_indptr, y=y, n_val_gs=n_val_gs, 
-            max_depth=max_depth, epsilon=epsilon, f=f, n_epochs=n_epochs, 
+        SPP(X_binned_data=X_binned_data, X_binned_indices=X_binned_indices,
+            X_binned_indptr=X_binned_indptr, y=y, n_val_gs=n_val_gs,
+            max_depth=max_depth, epsilon=epsilon, f=f, n_epochs=n_epochs,
             tol=tol, screening=screening, store_history=store_history)
 
     beta_star_spp = solutions_dict['spp_lasso_slopes']
