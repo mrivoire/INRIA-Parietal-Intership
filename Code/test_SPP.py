3676408922377ddff9ba3faab8de1edd9272dff1
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2020-09-06 17:18:02 +0200

    put compute interaction in separate function

--- Code/test_SPP.py
+++ Code/test_SPP.py
@@ -13,6 +13,53 @@ from SPP import simu, SPP, compute_interactions, max_val
 from scipy.sparse import csc_matrix, hstack
 
 
+def _compute_all_interactions(X_binned):
+    n_samples = X_binned.shape[0]
+    X_binned_data = X_binned.data
+    X_binned_indices = X_binned.indices
+    X_binned_indptr = X_binned.indptr
+
+    n_features = X_binned.shape[1]
+    X_inter_feat_data = []
+    X_inter_feat_ind = []
+    X_inter_feat_indptr = []
+    indptr = 0
+    X_inter_feat_indptr.append(indptr)
+    n_inter_feats = 0
+
+    # Computation of the features of interactions
+    X_tilde_keys = []
+
+    # Why testing with sklearn.preprocessing.PolynomialFeatures(degree=2, interaction_only=True, include_bias=False) ?
+    for i in range(n_features):
+        X_tilde_keys.append([i])
+        start_feat1, end_feat1 = X_binned_indptr[i: i + 2]
+        for j in range(i, n_features):
+            n_inter_feats += 1
+            start_feat2, end_feat2 = X_binned_indptr[j: j + 2]
+
+            inter_feat_data, inter_feat_ind = \
+                compute_interactions(
+                    data1=X_binned_data[start_feat1: end_feat1],
+                    ind1=X_binned_indices[start_feat1: end_feat1],
+                    data2=X_binned_data[start_feat2: end_feat2],
+                    ind2=X_binned_indices[start_feat2: end_feat2])
+
+            X_tilde_keys.append([i, j])
+            indptr += len(inter_feat_ind)
+
+            X_inter_feat_data.extend(inter_feat_data)
+            X_inter_feat_ind.extend(inter_feat_ind)
+            X_inter_feat_indptr.append(indptr)
+
+    X_interfeats = csc_matrix(
+        (X_inter_feat_data, X_inter_feat_ind, X_inter_feat_indptr),
+        shape=(n_samples, n_inter_feats))
+
+    X_tilde = hstack([X_binned, X_interfeats])
+    return X_tilde, X_tilde_keys
+
+
 @pytest.mark.parametrize("seed", [0, 1, 2, 5, 10])
 def test_SPP(seed):
 
@@ -54,43 +101,10 @@ def test_SPP(seed):
     # binning process of the original continuous features and the features of
     # interactions until the maximum order
 
-    X_inter_feat_data = []
-    X_inter_feat_ind = []
-    X_inter_feat_indptr = []
-    indptr = 0
-    X_inter_feat_indptr.append(indptr)
-    n_inter_feats = 0
+    X_tilde, X_tilde_keys = _compute_all_interactions(X_binned)
+    n_tilde_feats = X_tilde.shape[1]
+    print('n_tilde_feats = ', n_tilde_feats)
 
-    # Computation of the features of interactions
-    X_tilde_keys = []
-
-    # Why testing with sklearn.preprocessing.PolynomialFeatures(degree=2, interaction_only=True, include_bias=False) ?
-    for i in range(n_features):
-        X_tilde_keys.append([i])
-        start_feat1, end_feat1 = X_binned_indptr[i: i + 2]
-        for j in range(i, n_features):
-            n_inter_feats += 1
-            start_feat2, end_feat2 = X_binned_indptr[j: j + 2]
-
-            inter_feat_data, inter_feat_ind = \
-                compute_interactions(
-                    data1=X_binned_data[start_feat1: end_feat1],
-                    ind1=X_binned_indices[start_feat1: end_feat1],
-                    data2=X_binned_data[start_feat2: end_feat2],
-                    ind2=X_binned_indices[start_feat2: end_feat2])
-
-            X_tilde_keys.append([i, j])
-            indptr += len(inter_feat_ind)
-
-            X_inter_feat_data.extend(inter_feat_data)
-            X_inter_feat_ind.extend(inter_feat_ind)
-            X_inter_feat_indptr.append(indptr)
-
-    X_interfeats = csc_matrix(
-        (X_inter_feat_data, X_inter_feat_ind, X_inter_feat_indptr),
-        shape=(n_samples, n_inter_feats))
-
-    X_tilde = hstack([X_binned, X_interfeats])
     X_tilde_data = X_tilde.data
     X_tilde_indptr = X_tilde.indptr
     X_tilde_ind = X_tilde.indices
@@ -101,9 +115,6 @@ def test_SPP(seed):
                                   residuals=y, max_depth=max_depth)
 
     lmbda = lambda_max / 2
-
-    n_tilde_feats = n_features + n_inter_feats
-    print('n_tilde_feats = ', n_tilde_feats)
 
     # (beta_star_lasso, residuals, primal_hist_sparse, dual_hist_sparse,
     #  gap_hist_sparse, r_list_sparse, n_active_features_true_sparse,
